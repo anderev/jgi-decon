1957383d83f997eb3d913e3b7c867102cd876832
Evan Andersen <ewandersen@lbl.gov>
2014-05-06 19:52:20 -0700

    Uses checkboxes for boolean. Refactoring.

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,16 @@ var spawn = require('child_process').spawn;
 var fs = require('fs');
 
 db.serialize(function() {
+  var install_location = '/global/homes/e/ewanders/scd-1.3.1';
+  var nt_location = '/global/dna/shared/rqc/ref_databases/ncbi/CURRENT/nt/nt';
+  var working_dir = '/global/homes/e/ewanders/dev/scd-viz-3/working_dirs';
+  db.run("CREATE TABLE IF NOT EXISTS config (config_id INTEGER PRIMARY KEY, install_location TEXT, nt_location TEXT, working_dir TEXT)", function(err) {
+    if(!err) {
+      db.run("INSERT OR IGNORE INTO config VALUES (1,?,?,?)", [install_location, nt_location, working_dir]);
+    }
+  });
   db.run("CREATE TABLE IF NOT EXISTS project (project_id INTEGER PRIMARY KEY AUTOINCREMENT, taxon_display_name TEXT, taxon_domain TEXT, taxon_phylum TEXT, taxon_class TEXT, taxon_order TEXT, taxon_family TEXT, taxon_genus TEXT, taxon_species TEXT)");
-  db.run("CREATE TABLE IF NOT EXISTS job (job_id INTEGER PRIMARY KEY AUTOINCREMENT, project_id INT, process_id INT, start_time INT, end_time INT, install_location TEXT, nt_location TEXT, working_dir TEXT, in_fasta TEXT, job_name TEXT, run_genecall INT, run_blast INT, run_classify INT, run_accuracy INT, blast_threads INT)");
+  db.run("CREATE TABLE IF NOT EXISTS job (job_id INTEGER PRIMARY KEY AUTOINCREMENT, project_id INT, process_id INT, start_time INT, end_time INT, working_dir TEXT, in_fasta TEXT, job_name TEXT, run_genecall INT, run_blast INT, run_classify INT, run_accuracy INT, blast_threads INT)");
 });
 
 // GET
@@ -103,27 +111,34 @@ exports.addProject = function(req, res) {
 };
 
 exports.addJob = function(req, res) {
+  var config;
+  db.get("SELECT * FROM config", function(err,row) {
+    if(!err) {
+      config = row;
+    } else {
+      console.log("Failed to read config from database.");
+      res.json(false);
+    }});
   var now = new Date();
   var start_time = now.toDateString() + ' ' + now.toTimeString();
-  var install_location = '/global/homes/e/ewanders/scd-1.3.1';
-  var nt_location = '/global/dna/shared/rqc/ref_databases/ncbi/CURRENT/nt/nt';
-  db.run("INSERT INTO job VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", [req.body.project_id,null,start_time,null,install_location,nt_location,req.body.working_dir,req.body.in_fasta,'',req.body.run_genecall,req.body.run_blast,req.body.run_classify,req.body.run_accuracy,req.body.blast_threads], function(err) {
+  db.run("INSERT INTO job VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?)", [req.body.project_id,null,start_time,null,null,req.body.in_fasta,'',req.body.run_genecall,req.body.run_blast,req.body.run_classify,req.body.run_accuracy,req.body.blast_threads], function(err) {
     if(!err) {
       db.get("SELECT last_insert_rowid()", function(err,row) {
         if(row) {
           var job_id = row['last_insert_rowid()'];
           db.get("SELECT * FROM job natural join project WHERE job_id = ?", [job_id], function(err, row) {
             if(row) {
-              var working_dir = '/global/homes/e/ewanders/dev/scd-viz-3/working_dirs';
-              row.working_dir = working_dir;
-              row.job_name = 'job_'+job_id;
-              row.install_location = install_location;
+              var cfg = row;
+              cfg.working_dir = config.working_dir;
+              cfg.job_name = 'job_'+job_id;
+              cfg.install_location = config.install_location;
+              cfg.nt_location = config.nt_location;
               var config_data = "";
               var config_keys = ['taxon_display_name','taxon_domain','taxon_phylum','taxon_class','taxon_order','taxon_family','taxon_genus','taxon_species','install_location','nt_location','working_dir','in_fasta','job_name','run_genecall','run_blast','run_classify','run_accuracy','blast_threads'];
               for (var k=0; k<config_keys.length; ++k) {
                 var key = config_keys[k];
-                var value = row[key];
-                if(value || (key == 'nt_location' || key.indexOf('run_' >= 0) || key.indexOf('taxon_') >= 0)) {
+                var value = cfg[key];
+                if(value || (key.indexOf('run_' >= 0) || key.indexOf('taxon_') >= 0)) {
                   config_data = config_data + key.toUpperCase() + '=';
                   if(value != null) {
                     config_data = config_data + '"' + value + '"\n';
@@ -133,14 +148,14 @@ exports.addJob = function(req, res) {
                 } else {
                   res.json(false);
                   console.log(key+'='+value);
-                  console.log(row);
+                  console.log(cfg);
                   return;
                 }
               }
-              if(!fs.existsSync(working_dir)) {
-                fs.mkdirSync(working_dir);
+              if(!fs.existsSync(cfg.working_dir)) {
+                fs.mkdirSync(cfg.working_dir);
               }
-              var new_config_filename = working_dir+'/'+row.job_name+'_config.cnf';
+              var new_config_filename = cfg.working_dir+'/'+cfg.job_name+'_config.cnf';
               var new_config = fs.openSync(new_config_filename, 'w');
               fs.writeSync(new_config, config_data);
               fs.closeSync(new_config);
@@ -156,7 +171,7 @@ exports.addJob = function(req, res) {
                 console.log('child process exited with status: ' + code);
               });
               var pid = process.pid;
-              db.run("UPDATE job SET process_id = ?, working_dir = ?, job_name = ? WHERE job_id = ?", pid, working_dir, row.job_name, job_id, function(err) {
+              db.run("UPDATE job SET process_id = ?, working_dir = ?, job_name = ? WHERE job_id = ?", pid, cfg.working_dir+'/'+cfg.job_name, cfg.job_name, job_id, function(err) {
                 if(!err) {
                   res.json(req.body);
                 } else {
