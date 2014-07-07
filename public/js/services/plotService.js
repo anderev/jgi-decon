3fe3adc258f9a2b7f1e3985cc8db4539d1938440
Evan Andersen <ewandersen@lbl.gov>
2014-07-06 18:13:08 -0700

    Basic particle array implementation.

--- public/js/services/plotService.js
+++ public/js/services/plotService.js
@@ -21,16 +21,19 @@ angular.module('myApp.services').service('plotService', function() {
 
   this.init = function(data) {
     var plot_area = document.getElementById("plot_area");
-    var renderer = new THREE.WebGLRenderer({clearAlpha:1});
+    //var renderer = new THREE.WebGLRenderer({clearAlpha:1});
+    var renderer = new THREE.CanvasRenderer();
     var scene = new THREE.Scene();
     var width = 512;
     var height = 512;
     var camera = new THREE.PerspectiveCamera(60, width/height, 0.0001, 1000);
+    var projector = new THREE.Projector();
 
     renderer.setSize(width, height);
     plot_area.appendChild(renderer.domElement);
 
     var controls = new THREE.TrackballControls(camera, renderer.domElement);
+    var PI2 = Math.PI * 2;
 
     controls.rotateSpeed = 5.0;
     controls.zoomSpeed = 1.0;
@@ -41,35 +44,26 @@ angular.module('myApp.services').service('plotService', function() {
     controls.dynamicDampingFactor = 0.3;
     controls.keys = [65, 83, 68];
 
-    var vertexShaderSource = '\
-    	attribute vec3 color;\
-    	varying vec3 vColor;\
-    	void main() {\
-    		vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );\
-    		gl_Position = projectionMatrix * mvPosition;\
-    		gl_PointSize = 32.0;\
-    		vColor = color;\
-        }';
-    var fragmentShaderSource = '\
-    	varying vec3 vColor;\
-    	void main() {\
-                vec2 r = gl_PointCoord - vec2(0.5,0.5);\
-                if(length(r) <= 0.3) {\
-                  gl_FragColor = vec4(vColor, 1.0);\
-                } else if(length(r) <= 0.5) {\
-                  gl_FragColor = vec4(0,0,0, 0.75*(1.0 - 5.0*(length(r)-0.3)));\
-                } else {\
-                  gl_FragColor = vec4(0,0,0,0.0);\
-                }\
-        }';
-    var attributes = {
-    	color: { type: 'c', value: []}
-    };
+    var programFill = function(ctx) {
+      ctx.beginPath();
+      ctx.arc( 0, 0, 0.5, 0, PI2, true );
+      ctx.fill();
+    }
+
+    var programStroke = function(ctx) {
+      ctx.lineWidth = 0.025;
+      ctx.beginPath();
+      ctx.arc( 0, 0, 0.5, PI2, true );
+      ctx.stroke();
+    }
+
+    var mouse = {x:0, y:0};
+
     var particles = new THREE.Geometry();
     for(var p_i=0; p_i<data.points.length; ++p_i) {
     	var p = data.points[p_i];
         var typeColor = null;
-    	particles.vertices.push(new THREE.Vector3(p.x, p.y, p.z));
+    	//particles.vertices.push(new THREE.Vector3(p.x, p.y, p.z));
         if( p.name.match(/clean/g) ) {
           typeColor = new THREE.Color(0,1,0);
         } else if( p.name.match(/hybrid/g) ) {
@@ -79,17 +73,13 @@ angular.module('myApp.services').service('plotService', function() {
         } else  {
           typeColor = new THREE.Color(1,1,0);
         }
-        attributes.color.value.push(typeColor);
+        var particle = new THREE.Sprite( new THREE.SpriteCanvasMaterial( {color: typeColor, program: programFill} ) );
+        particle.position.x = p.x;
+        particle.position.y = p.y;
+        particle.position.z = p.z;
+        particle.scale.x = particle.scale.y = 0.001;
+        scene.add( particle );
     }
-    var material = new THREE.ShaderMaterial({
-    	attributes: attributes,
-    	vertexShader: vertexShaderSource,
-    	fragmentShader: fragmentShaderSource,
-        transparent: true
-    });
-    var particleSystem = new THREE.ParticleSystem(particles, material);
-    particleSystem.sortParticles = true;
-    scene.add(particleSystem);
 
     addAxes(scene);
 
@@ -106,9 +96,17 @@ angular.module('myApp.services').service('plotService', function() {
     };
 
     controls.addEventListener('change', render);
+    plot_area.addEventListener('mousemove', onPlotMouseMove, false);
 
     animate();
   };
+
+  function onPlotMouseMove(event) {
+    event.preventDefault();
+    mouse.x = (event.clientX / plot_area.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / plot_area.innerHeight) * 2 + 1;
+  }
+
 });
 
   
