838dfa76f6fa626e04b7fbb30a2ddecf2b6b4218
Evan Andersen <ewandersen@lbl.gov>
2014-10-01 11:57:13 -0700

    Updates UI config to use SCD 1.3.4.

--- ui/config.js
+++ ui/config.js
@@ -1,18 +1,18 @@
 var staging = {
-  install_location: '/global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.3',
-  nt_location: '/global/dna/shared/rqc/ref_databases/ncbi/CURRENT/nt/nt',
+  install_location: '/global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4',
+  nt_location: '',
   working_dir: '/global/homes/e/ewanders/dev/scd-viz/working_dirs',
-  scd_exe: 'qsub -N JOBNAME -j y -R y -V -o LOGFILE -l h_rt=12:00:00 -l ram.c=48G -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.3/bin/scd.sh',
+  scd_exe: 'qsub -N JOBNAME -j y -R y -V -o LOGFILE -l h_rt=12:00:00 -l ram.c=48G -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4/bin/scd.sh',
   env: global.process.env.NODE_ENV || 'staging',
   port: 8091
 };
 
   
 var production = {
-  install_location: '/global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.3',
-  nt_location: '/global/dna/shared/rqc/ref_databases/ncbi/CURRENT/nt/nt',
+  install_location: '/global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4',
+  nt_location: '',
   working_dir: '/global/homes/e/ewanders/prod/scd-viz/working_dirs',
-  scd_exe: 'qsub -N JOBNAME -j y -R y -V -o LOGFILE -l h_rt=12:00:00 -l ram.c=48G -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.3/bin/scd.sh',
+  scd_exe: 'qsub -N JOBNAME -j y -R y -V -o LOGFILE -l h_rt=12:00:00 -l ram.c=48G -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4/bin/scd.sh',
   env: global.process.env.NODE_ENV || 'production',
   port: 3051
 };
