f9a40fa122e4e2b9f6b0bedf4e7f30d171894f59
Evan Andersen <ewandersen@lbl.gov>
2014-10-02 10:46:40 -0700

    Removes 48G ram requirement for qsub jobs.

--- ui/config.js
+++ ui/config.js
@@ -2,7 +2,7 @@ var staging = {
   install_location: '/global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4',
   nt_location: '',
   working_dir: '/global/homes/e/ewanders/dev/scd-viz/working_dirs',
-  scd_exe: 'qsub -N JOBNAME -j y -R y -V -o LOGFILE -l h_rt=12:00:00 -l ram.c=48G -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4/bin/scd.sh',
+  scd_exe: 'qsub -N JOBNAME -j y -V -o LOGFILE -l exclusive.c -l h_rt=12:00:00 -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4/bin/scd.sh',
   env: global.process.env.NODE_ENV || 'staging',
   port: 8091
 };
@@ -12,7 +12,7 @@ var production = {
   install_location: '/global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4',
   nt_location: '',
   working_dir: '/global/homes/e/ewanders/prod/scd-viz/working_dirs',
-  scd_exe: 'qsub -N JOBNAME -j y -R y -V -o LOGFILE -l h_rt=12:00:00 -l ram.c=48G -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4/bin/scd.sh',
+  scd_exe: 'qsub -N JOBNAME -j y -V -o LOGFILE -l exclusive.c -l h_rt=12:00:00 -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4/bin/scd.sh',
   env: global.process.env.NODE_ENV || 'production',
   port: 3051
 };
