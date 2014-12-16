f1efa86e1f544af1e541a0668cbd8ebddb4395c9
Kristin Tennessen <ktennessen@lbl.gov>
2014-12-15 16:21:12 -0800

    Changed tax script from python to perl

--- src/bin/03.createTaxSpeciesfile.sh
+++ src/bin/03.createTaxSpeciesfile.sh
@@ -3,8 +3,9 @@ cdir=$1
 ntdir=$2
 ntloc=$3
 
-python ${cdir}/bin/03.createTaxSpeciesfile.py $ntloc ${ntdir}/ncbi_taxonomy_leafnodes_species.out
+perl ${cdir}/bin/03.createTaxSpeciesfile.pl $ntloc ${ntdir}/ncbi_taxonomy_leafnodes_species.out
 
 rm $ntloc/*.dmp
 rm $ntloc/*.prt
 rm $ntloc/readme.txt
+rm $ntloc/*gz
