03379ea3b2d86ea1a79c00f52aaca1f49fda35bc
Evan Andersen <ewandersen@lbl.gov>
2015-04-08 21:21:51 -0700

    Adds new site logic for move to gpweb35.

--- ui/config.js
+++ ui/config.js
@@ -1,19 +1,25 @@
+var spawn = require('child_process').spawn;
 var process = require('process');
 var https = require('https');
-var http = require('http');
+var fs = require('fs');
+var Q = require('q');
+var ssh_client = require('node-sshclient');
 
 var Production = function() {
-  this.install_location = '/global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4';
+  this.install_location = '/global/projectb/sandbox/omics/sc-decontamination/Production/prodege-2.0';
   this.nt_location = '';
-  this.scd_exe = 'qsub -N JOBNAME -j y -V -o LOGFILE -l exclusive.c -l h_rt=12:00:00 -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/scd-1.3.4/bin/scd.sh';
-  this.bundle_src = '/global/projectb/sandbox/omics/sc-decontamination/Releases/scd-1.3.4.tgz';
-  this.bundle_src_name = 'prodege-1.3.4.tgz';
+  this.bundle_src = './prodege-2.0.tgz';
+  this.bundle_src_name = 'prodege-2.0.tgz';
 
   //Overrides
-  this.working_dir = '/global/homes/g/gbp/ProDeGe/working_dirs';
+  this.job_start = function(job_id, temp_config_filename, temp_fasta_filename, user_id, cb) { return job_start(job_id, temp_config_filename, temp_fasta_filename, user_id, this.remote_working_dir, cb); };
+  this.job_status = function(job_id, process_id, user_id, cb) { job_status(job_id, process_id, user_id, this.local_working_dir, cb); };
+  this.remote_working_dir = '/global/projectb/scratch/ewanders/prodege/production';
+  this.local_working_dir = '/dna/shared/data/gbp/prodege/production';
+  this.upload_dir = process.cwd()+'/uploads';
   this.env = 'production';
   this.port = 3051;
-  this.caliban_return_URL = 'http://prodege.jgi-psf.org/';
+  this.caliban_return_URL = 'https://prodege.jgi-psf.org/';
   this.caliban_signon_URL = 'https://signon2.jgi-psf.org';
   this.caliban_signoff_URL = 'https://signon.jgi-psf.org/signon/destroy';
   this.caliban_api_users = function(user_id, cb) { return https.get({hostname:'signon.jgi-psf.org', path: '/api/users/'+user_id}, cb); };
@@ -24,17 +30,141 @@ var Production = function() {
 var Staging = function() {
   Production.call(this);
 
-  this.working_dir = process.cwd()+'/working_dirs';
+  this.remote_working_dir = '/global/projectb/scratch/ewanders/prodege/staging';
+  this.local_working_dir = '/dna/shared/data/gbp/prodege/staging';
   this.env = 'staging';
-  this.port = 80;
-  this.caliban_return_URL = 'http://staging.localdomain/';
-  this.caliban_signon_URL = 'http://staging.localdomain/caliban/signon';
-  this.caliban_signoff_URL = 'http://staging.localdomain/caliban/signoff';
-  this.caliban_api_users = function(user_id, cb) { return http.get({hostname:'staging.localdomain', path: '/caliban/api/users/'+user_id}, cb); };
-  this.caliban_api_session = function(session, cb) { return http.get({hostname:'staging.localdomain', path: '/caliban'+session}, cb); };
-  this.caliban_cookie_domain = 'staging.localdomain';
+  this.caliban_return_URL = 'https://prodege-dev.jgi-psf.org/';
 };
 Staging.prototype = Object.create(Production.prototype);
 Staging.prototype.constructor = Staging;
 
+function ssh_promise(command) {
+  console.log('ssh gp '+command);
+  var deferred = Q.defer();
+  var ssh = new ssh_client.SSH({hostname: 'gp'});
+  ssh.command(command, function(result) {
+    if( result.exitCode === 0 ) {
+      console.log('ssh success');
+      deferred.resolve(result.stdout);
+    } else {
+      console.log('ssh fail');
+      deferred.reject(result.stderr);
+    }
+  });
+  return deferred.promise;
+}
+
+function scp_promise(src, dest) {
+  console.log('scp '+src+' @gp:'+dest);
+  var deferred = Q.defer();
+  var scp = new ssh_client.SCP({hostname: 'gp'});
+  scp.upload(src, dest, function(result) {
+    if( result.exitCode === 0 ) {
+      console.log('scp success');
+      deferred.resolve(result.stdout);
+    } else {
+      console.log('scp fail');
+      deferred.reject(result.stderr);
+    }
+  });
+  return deferred.promise;
+}
+
+function qsub_promise(command) {
+  var deferred = Q.defer();
+  ssh_promise(command)
+  .then(function(output) {
+    var parsed = (new String(output)).match(/Your job ([0-9]+)/);
+    if(parsed.length == 2) {
+      var process_id = parseInt(parsed[1]);
+      deferred.resolve(process_id);
+    } else {
+      deferred.reject('Failed to parse qsub job');
+    }
+  }, function(reason) {
+    deferred.reject(reason);
+  });
+
+  return deferred.promise;
+}
+
+function job_status(jid, pid, user_id, working_dir, cb) {
+  ssh_promise('/usr/common/usg/bin/qs -j '+pid+' --style json')
+  .then(function(output) {
+    var parsed_job_status = JSON.parse(new String(output).split('\n')[0]);
+    console.log('parsed_job_status: '+parsed_job_status);
+    if(parsed_job_status.length === 1) {
+      var str_status = '';
+      if(parsed_job_status[0].state.match(/qw/)) {
+        str_status = 'Waiting in Queue';
+      } else if(parsed_job_status[0].state.match(/r/)) {
+        str_status = 'Running';
+      } else if(parsed_job_status[0].state.match(/t/)) {
+        str_status = 'Transfer';
+      } else {
+        str_status = 'Unknown';
+      }
+      if(parsed_job_status[0].state.match(/E/)) {
+        str_status = str_status + ' (ERROR)';
+      }
+      if(parsed_job_status[0].state.match(/h/)) {
+        str_status = str_status + ' (HOLD)';
+      }
+      if(parsed_job_status[0].state.match(/R/)) {
+        str_status = str_status + ' (RESUBMITTED)';
+      }
+      cb(null, str_status);
+    } else {
+      console.log('no current job');
+      console.log('checking: '+working_dir+'/sso_'+user_id+'/job_'+jid);
+      fs.exists(working_dir+'/sso_'+user_id+'/job_'+jid, function(exists) {
+        if(exists) {
+          console.log('exists');
+          cb(null, 'Complete');
+        } else {
+          console.log('not exists');
+          cb(null, 'Unknown');
+        }
+      });
+    }
+  }, function(reason) {
+    cb(reason, null);
+  });
+}
+
+function job_start(job_id, temp_config_filename, temp_fasta_filename, user_id, working_dir, cb) {
+  var user_working_dir = working_dir+'/sso_'+user_id;
+  var remote_config_filename = user_working_dir+'/job_'+job_id+'.cnf';
+  var remote_fasta_filename = user_working_dir+'/job_'+job_id+'.fna';
+  ssh_promise('mkdir -p '+user_working_dir)
+    .then(function() {
+      console.log('User dir exists: '+user_working_dir);
+      Q.allSettled([
+        scp_promise( temp_config_filename, remote_config_filename ),
+        scp_promise( temp_fasta_filename, remote_fasta_filename )
+        ]).spread(function(config, fasta) {
+          console.log('scp successful');
+          var cmd_line = 'qsub -N $JOBNAME -j y -V -o $LOGFILE -l exclusive.c -l h_rt=12:00:00 -pe pe_slots 8 /global/projectb/sandbox/omics/sc-decontamination/Production/prodege-2.0/bin/prodege.sh $CONF_FILE'
+          .replace('$JOBNAME', 'SCD-job_'+job_id)
+          .replace('$LOGFILE', user_working_dir+'/job_'+job_id+'-qsub.log')
+          .replace('$CONF_FILE', remote_config_filename);
+          qsub_promise(cmd_line)
+          .then(function(jid) {
+            console.log('compute job: ' + jid);
+            qsub_promise('qsub -q xfer.q -hold_jid '+jid+' ~/rsync_prodege_data.sh ~/rsync_'+jid+'.log')
+            .then(function(jid) { console.log('rsync job: '+jid); cb(null, jid); }, function(reason) { cb(reason, null); });
+          },
+          function(reason) {
+            cb(reason, null);
+          });
+        }, function(scp_rejection) {
+          console.log(scp_rejection);
+        });
+    }, function(reason) {
+      console.log('User mkdir failed: '+user_working_dir);
+      console.log(reason);
+    });
+}
+
 exports.Config = global.process.env.NODE_ENV === 'production' ? new Production() : new Staging();
+
--- ui/routes/api.js
+++ ui/routes/api.js
@@ -1,22 +1,18 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('scd.db');
-var spawn = require('child_process').spawn;
 var fs = require('fs');
 var config = require('../config').Config;
 var parser = require('./parsers');
 var stats = require('./stats');
 var caliban = require('./caliban');
+var temp = require('temp').track();
 
-/*
-db.on('trace', function(query) {
-  console.log('SQLite: ' + query);
-});
-*/
+var user_uploads = {};
 
-fs.exists(config.working_dir, function(exists) {
+fs.exists(config.upload_dir, function(exists) {
   if(!exists) {
-    console.log('Creating ' + config.working_dir);
-    fs.mkdir(config.working_dir, function(err) {
+    console.log('Creating ' + config.upload_dir);
+    fs.mkdir(config.upload_dir, function(err) {
       if(err) {
         console.log(err);
       }
@@ -55,6 +51,10 @@ exports.jobs = function(req, res) {
               --id_count;
               if(id_count == 0) {
                 jobs.sort(function(job_a, job_b) {
+                  if(job_a.notes === null || job_b.notes === null) {
+                    return job_b.job_id - job_a.job_id;
+                  }
+
                   if(job_a.notes.match(/endo-.*/) && !job_b.notes.match(/endo-.*/)) {
                     return -1;
                   } else if (!job_a.notes.match(/endo-.*/) && job_b.notes.match(/endo-.*/)) {
@@ -101,49 +101,15 @@ exports.job = function(req, res) {
       var id = req.params.id;
       db.get("SELECT * FROM job WHERE job_id = ? AND (user_id = ? OR is_public = 1)", [id, user.id[0]], function(err, row) {
         if(!err && row) {
-          var process = spawn('qs', ['-j', row.process_id, '--style', 'json']);
-          process.stdout.on('data', function(data) {
-            //console.log('qs stdout: ' + data);
-            var status = JSON.parse(new String(data).split('\n')[0]);
-            if(status.length == 1) {
-              if(status[0].state.match(/qw/)) {
-                row.process_status = 'Waiting in Queue';
-              } else if(status[0].state.match(/r/)) {
-                row.process_status = 'Running';
-              } else if(status[0].state.match(/t/)) {
-                row.process_status = 'Transfer';
-              } else {
-                row.process_status = 'Unknown';
-              }
-              if(status[0].state.match(/E/)) {
-                row.process_status = row.process_status + ' (ERROR)';
-              }
-              if(status[0].state.match(/h/)) {
-                row.process_status = row.process_status + ' (HOLD)';
-              }
-              if(status[0].state.match(/R/)) {
-                row.process_status = row.process_status + ' (RESUBMITTED)';
-              }
-              res.json({ job: row });
+          config.job_status(row.job_id, row.process_id, row.user_id, function(err, str_status) {
+            if(!err) {
+              row.process_status = str_status;
+              res.json({job: row});
             } else {
-              fs.exists(config.working_dir+'/sso_'+user.id[0]+'/job_'+row.job_id, function(exists) {
-                if(exists) {
-                  row.process_status = 'Complete';
-                } else {
-                  row.process_status = 'unknown';
-                }
-                res.json({ job: row });
-              });
+              console.log('error: '+err);
+              res.json(false);
             }
           });
-          process.stderr.on('data', function(data) {
-            console.log('qs stderr: ' + data);
-          });
-          /*
-          process.on('close', function(code) {
-            console.log('qs exited with status: ' + code);
-          });
-          */
         } else {
           res.json(false);
         }
@@ -187,7 +153,7 @@ exports.parseJobFiles = function(req, res, cb_ok, cb_err) {
         if(!err) {
           if( user.id[0] == row.user_id || row.is_public ) {
             var contigs = [];
-            var workingDir = config.working_dir + '/sso_' + row.user_id;
+            var workingDir = config.local_working_dir + '/sso_' + row.user_id;
             var job_name = 'job_' + req.params.id;
             var intermediate_dir = workingDir + '/' + job_name + '/' + job_name + '_Intermediate/';
             fs.readdir(intermediate_dir, function(err, files) {
@@ -270,21 +236,20 @@ exports.parseJobFiles = function(req, res, cb_ok, cb_err) {
     }
   });
 }
+
 // POST
 exports.uploadFasta = function(req, res) {
   caliban.getSessionUser(req, function(user_err, user) {
     if(!user_err) {
-      var fstream;
       req.pipe(req.busboy);
       req.busboy.on('file', function(fieldname, file, filename) {
-        var newFilename = config.working_dir + '/sso_' + user.id[0] + '/upload.fasta';
         console.log('Receiving file: ' + filename);
-        fstream = fs.createWriteStream(newFilename);
+        var fstream = temp.createWriteStream();
         file.pipe(fstream);
         fstream.on('close', function() {
-          console.log('Finished receiving: ' + filename);
+          user_uploads[user.id[0]] = fstream.path;
+          console.log('Finished receiving: ' + filename + ' at ' + fstream.path );
           res.json(req.body);
-          //res.redirect('back');
         });
       });
     } else {
@@ -312,120 +277,81 @@ exports.addJob = function(req, res) {
             db.get("SELECT * FROM job WHERE job_id = ? AND user_id = ?", [job_id, user.id[0]], function(err, row) {
               if(row) {
                 var cfg = row;
-                if(!req.body.in_fasta) {
-                  var uploadedFilename = config.working_dir + '/sso_' + user.id[0] + '/upload.fasta';
-                  fs.exists(uploadedFilename, function(exists) {
-                    if(exists) {
-                      var permanentFilename = config.working_dir + '/sso_' + user.id[0] + '/uploaded_job_' + job_id + '.fasta';
-                      console.log('rename: ' + uploadedFilename + ' => ' + permanentFilename);
-                      fs.rename(uploadedFilename, permanentFilename, function(err) {
-                        if(err) {
-                          console.log(err);
-                          res.json(false);
+                var temp_fasta_filename = user_uploads[user.id[0]];
+
+                fs.exists(temp_fasta_filename, function(exists) {
+                  if(exists) {
+                    stats.parse_fna(temp_fasta_filename, function(fasta_stats) {
+                      db.run("UPDATE job SET gc_percent = ?, num_bases = ?, num_contigs = ? WHERE job_id = ?", [fasta_stats.gc_percent, fasta_stats.num_bases, fasta_stats.num_contigs, job_id], function(err) {
+                        if(!err) {
+                          console.log('Updated job_id ' + job_id + ' with ' + JSON.stringify(fasta_stats));
                         } else {
-                          prepareJob(permanentFilename);
+                          console.log(err);
                         }
                       });
-                    } else {
-                      console.log('Missing in_fasta.');
-                      res.json(false);
-                    }
-                  });
-                } else {
-                  prepareJob(req.body.in_fasta);
-                }
-
-                function prepareJob(in_fasta) {
-                  stats.parse_fna(in_fasta, function(fasta_stats) {
-                    db.run("UPDATE job SET gc_percent = ?, num_bases = ?, num_contigs = ? WHERE job_id = ?", [fasta_stats.gc_percent, fasta_stats.num_bases, fasta_stats.num_contigs, job_id], function(err) {
-                      if(!err) {
-                        console.log('Updated job_id ' + job_id + ' with ' + JSON.stringify(fasta_stats));
-                      } else {
-                        console.log(err);
-                      }
+                    }, function(err) {
+                      console.log('Error getting stats on fasta: ' + temp_fasta_filename);
                     });
-                  }, function(err) {
-                    console.log('Error getting stats on fasta: ' + in_fasta);
-                  });
-                  cfg.in_fasta = in_fasta;
-                  cfg.working_dir = config.working_dir + '/sso_' + user.id[0];
-                  cfg.job_name = 'job_'+job_id;
-                  cfg.install_location = config.install_location;
-                  cfg.nt_location = config.nt_location;
-                  cfg.run_genecall = 1;
-                  cfg.run_blast = 1;
-                  cfg.run_classify = 1;
-                  cfg.run_accuracy = 0;
-                  cfg.blast_threads = 8;
-                  var config_data = "";
-                  var config_keys = ['taxon_display_name','taxon_domain','taxon_phylum','taxon_class','taxon_order','taxon_family','taxon_genus','taxon_species','install_location','nt_location','working_dir','in_fasta','job_name','run_genecall','run_blast','run_classify','run_accuracy','blast_threads'];
-                  for (var k=0; k<config_keys.length; ++k) {
-                    var key = config_keys[k];
-                    var value = cfg[key];
-                    if(value || (key.indexOf('run_' >= 0) || key.indexOf('taxon_') >= 0)) {
-                      config_data = config_data + key.toUpperCase() + '=';
-                      if(value != null) {
-                        config_data = config_data + '"' + value + '"\n';
+                    cfg.working_dir = config.remote_working_dir + '/sso_' + user.id[0];
+                    cfg.in_fasta = cfg.working_dir + '/job_' + job_id + '.fna';
+                    cfg.job_name = 'job_'+job_id;
+                    cfg.install_location = config.install_location;
+                    cfg.nt_location = config.nt_location;
+                    cfg.run_genecall = 1;
+                    cfg.run_blast = 1;
+                    cfg.run_classify = 1;
+                    cfg.run_accuracy = 0;
+                    cfg.blast_threads = 8;
+                    var config_data = "";
+                    var config_keys = ['taxon_display_name','taxon_domain','taxon_phylum','taxon_class','taxon_order','taxon_family','taxon_genus','taxon_species','install_location','nt_location','working_dir','in_fasta','job_name','run_genecall','run_blast','run_classify','run_accuracy','blast_threads'];
+                    for (var k=0; k<config_keys.length; ++k) {
+                      var key = config_keys[k];
+                      var value = cfg[key];
+                      if(value || (key.indexOf('run_' >= 0) || key.indexOf('taxon_') >= 0)) {
+                        config_data = config_data + key.toUpperCase() + '=';
+                        if(value != null) {
+                          config_data = config_data + '"' + value + '"\n';
+                        } else {
+                          config_data = config_data + '\n';
+                        }
                       } else {
-                        config_data = config_data + '\n';
+                        res.json(false);
+                        console.log(key+'='+value);
+                        console.log(cfg);
+                        return;
                       }
-                    } else {
-                      res.json(false);
-                      console.log(key+'='+value);
-                      console.log(cfg);
-                      return;
                     }
-                  }
 
-                  var startJob = function() {
-                    var new_config_filename = cfg.working_dir+'/'+cfg.job_name+'_config.cnf';
-                    console.log('Starting job for config: '+new_config_filename);
-                    fs.writeFile(new_config_filename, config_data, function(err) {
+                    temp.open('prodege-', function(err, info) {
                       if(!err) {
-                        var cmd_line = config.scd_exe.replace("JOBNAME", "SCD-"+cfg.job_name).replace("LOGFILE", cfg.working_dir+'/'+cfg.job_name+'-qsub.log')+' '+new_config_filename;
-                        var cmd_args = cmd_line.split(' ');
-                        var cmd_exe = cmd_args.shift();
-                        var process = spawn(cmd_exe, cmd_args);
-                        process.stdout.on('data', function(data) {
-                          console.log('stdout: ' + data);
-                          var parsed = (new String(data)).match(/Your job ([0-9]+)/);
-                          if(parsed.length == 2) {
-                            var process_id = parseInt(parsed[1]);
-                            console.log('Parsed job id: ' + process_id);
-                            db.run("UPDATE job SET process_id = ? WHERE job_id = ?", [process_id, job_id], function(err) {
+                        console.log('Starting job for config: '+info.path);
+                        fs.write(info.fd, config_data);
+                        fs.close(info.fd, function(err) {
+                          if(!err) {
+                            config.job_start(job_id, info.path, temp_fasta_filename, user.id[0], function(err, process_id) {
                               if(!err) {
-                                res.json(req.body);
+                                db.run("UPDATE job SET process_id = ? WHERE job_id = ?", [process_id, job_id], function(err) {
+                                  if(!err) {
+                                    res.json(req.body);
+                                  } else {
+                                    console.log('Failed to update job with process_id and working_dir.');
+                                    console.log(err);
+                                    res.json(false);
+                                  }
+                                });
                               } else {
-                                console.log('Failed to update job with process_id and working_dir.');
-                                console.log(err);
-                                res.json(false);
+                                console.log('job_start error: '+err);
                               }
                             });
                           }
                         });
-                        process.stderr.on('data', function(data) {
-                          console.log('stderr: ' + data);
-                        });
-                        process.on('close', function(code) {
-                          console.log('child process exited with status: ' + code);
-                        });
                       }
                     });
+                  } else {
+                    console.log('Missing fasta: '+temp_fasta_filename);
+                    res.json(false);
                   }
-
-                  fs.exists(cfg.working_dir, function(exists) {
-                    if(!exists) {
-                      fs.mkdir(cfg.working_dir, function(err) {
-                        if(!err) {
-                          startJob();
-                        }
-                      });
-                    } else {
-                      startJob();
-                    }
-                  });
-
-                }
+                });
               } else {
                 console.log('Failed to find new row.');
               }
